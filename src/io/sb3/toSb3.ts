63deee716c3c608efc73f8f094953787b90339d1
Florrie <towerofnix@gmail.com>
2020-02-13 18:35:25 -0400

    sb3: first functional sb3 output
    Check out the result of this:
    https://scratch.mit.edu/projects/109782038
    
    Still lots of things to do before sb3 is completely supported - monitor
    and more shadow block support, primarily - but this is a big milestone
    all the same.

--- src/io/sb3/toSb3.ts
+++ src/io/sb3/toSb3.ts
@@ -1,163 +1,313 @@
-const doesThisCodeHaveErrors = "not if I comment it out";
-export default doesThisCodeHaveErrors;
-
-/*
-import * as JSZip from "jszip";
+import Block from "../../Block";
+import Costume from "../../Costume";
+import Project, { TextToSpeechLanguage} from "../../Project";
+import Script from "../../Script";
+import Sound from "../../Sound";
+import Target, { Sprite, Stage } from "../../Target";
+import * as BlockInput from "../../BlockInput";
 import * as sb3 from "../sb3";
+import { OpCode } from "../../OpCode";
+import { generateId } from "../../util/id";
 
-import Project from "../../Project";
-import Target from "../../Target";
+interface ToSb3Options {}
 
-import List from "../../List";
-import Variable from "../../Variable";
+interface ToSb3Output {
+  json: string
+}
 
-export default async function toSb3(project: Project): Promise<ArrayBuffer> {
-  const outZip = new JSZip();
+export default function toSb3(
+  options: Partial<ToSb3Options> = {}
+): ToSb3Output {
+  const project: Project = this;
 
-  for (const target of [project.stage, ...project.sprites]) {
-    for (const costume of target.costumes) {
-      outZip.file(`${costume.md5}.${costume.ext}`, costume.asset);
+  interface BlockData {
+    blocks: {[key: string]: sb3.Block},
+    broadcasts: {[key: string]: string}
+  }
+
+  function applyBlockData(a: BlockData, b?: BlockData) {
+    if (b) {
+      Object.assign(a.blocks, b.blocks);
+      Object.assign(b.broadcasts, b.broadcasts);
     }
   }
 
-  outZip.file("project.json", JSON.stringify(getProjectJSON(project)));
-
-  return await outZip.generateAsync({ type: "arraybuffer" });
-}
-
-function extractCostumes(target: Target): sb3.Costume[] {
-  return target.costumes.map(costume => {
-    const sb3Costume: sb3.Costume = {
-      assetId: costume.md5,
-      name: costume.name,
-      md5ext: `${costume.md5}.${costume.ext}`,
-      dataFormat: costume.ext,
-      rotationCenterX: costume.centerX / 2,
-      rotationCenterY: costume.centerY / 2
+  function newBlockData(): BlockData {
+    return {
+      blocks: {},
+      broadcasts: {}
     };
-    return sb3Costume;
-  });
-}
+  }
 
-function extractSounds(target: Target): sb3.Sound[] {
-  return target.sounds.map(sound => {
-    const sb3Sound: sb3.Sound = {
-      assetId: sound.md5,
-      name: sound.name,
-      dataFormat: sound.ext,
-      format: "",
-      rate: sound.sampleRate,
-      sampleCount: sound.sampleCount,
-      md5ext: `${sound.md5}.${sound.ext}`
+  function serializeInputs(block: Block, options: {stage: Stage, target: Target}): {
+    inputs: {
+      [key: string]: sb3.BlockInput
+    },
+    fields: {
+      [key: string]: any
+    },
+    blockData: BlockData
+  } {
+    const inputs: {[key: string]: sb3.BlockInput} = {};
+    const fields: {[key: string]: any} = {};
+    const blockData = newBlockData();
+
+    const {stage, target} = options;
+
+    const fieldEntries = sb3.fieldTypeMap[block.opcode];
+
+    const makeShadowBlock = (opcode: OpCode, fields: {[key: string]: any}): string => {
+      const id = generateId();
+
+      blockData.blocks[id] = {
+        opcode,
+        next: null,
+        parent: block.id,
+
+        fields,
+        inputs: {},
+
+        shadow: true,
+        topLevel: false
+      };
+
+      return id;
     };
-    return sb3Sound;
-  });
-}
 
-function extractMonitors(targets: Target[]): sb3.Monitor[] {
-  return targets.flatMap((target: Target) => [
-    ...target.variables.map((variable: Variable) => {
-      const monitor: sb3.VariableMonitor = {
-        mode: variable.mode,
-        opcode: "data_variable",
-        params: {
-          VARIABLE: variable.name
-        },
-        value: variable.value,
-        spriteName: target.isStage ? null : target.name,
-        width: 0,
-        height: 0,
-        x: variable.x,
-        y: variable.y,
-        visible: variable.visible,
-        sliderMin: variable.sliderMin,
-        sliderMax: variable.sliderMax,
-        isDiscrete: variable.isDiscrete
-      };
-      return monitor;
-    }),
-    ...target.lists.map((list: List) => {
-      const monitor: sb3.ListMonitor = {
-        mode: "list",
-        opcode: "data_listcontents",
-        params: {
-          LIST: list.name
-        },
-        height: list.height === null ? 0 : list.height,
-        width: list.width === null ? 0 : list.width,
-        x: list.x,
-        y: list.y,
-        spriteName: target.isStage ? null : target.name,
-        value: list.value,
-        visible: list.visible
-      };
-      return monitor;
-    })
-  ]);
-}
-
-function getProjectJSON(project: Project): sb3.ProjectJSON {
-  const stage: sb3.Stage = {
-    name: project.stage.name,
-    isStage: true,
-    currentCostume: project.stage.costumeNumber,
-    videoState: project.videoOn ? "on" : "off",
-    videoTransparency: project.videoAlpha,
-    costumes: extractCostumes(project.stage),
-    sounds: extractSounds(project.stage),
-    volume: project.stage.volume,
-    tempo: project.tempo,
-    variables: project.stage.variables.reduce((variables: sb3.Stage["variables"], variable) => {
-      let varData: [string, string | number, boolean?] = [variable.name, variable.value];
-      if (variable.cloud) {
-        varData.push(true);
+    for (const [key, input] of Object.entries(block.inputs)) {
+      // Fields are stored as a plain [value, id?] pair.
+      if (fieldEntries && key in fieldEntries) {
+        let id: string;
+        switch (input.type) {
+          case "variable": {
+            let variable = target.getVariable(input.value);
+            variable = variable || stage.getVariable(input.value);
+            id = variable && variable.id;
+            break;
+          }
+          default:
+            id = null;
+        }
+        fields[key] = [input.value, id];
+        continue;
       }
-      return { ...variables, [variable.id]: varData };
-    }, {}),
-    lists: project.stage.lists.reduce(
-      (lists: sb3.Stage["lists"], list) => ({
-        ...lists,
-        [list.id]: [list.name, list.value]
+
+      // Inputs are stored as one of two different types of value:
+      // - A reference to a block (either a reporter or, in the case of C-shape
+      //   inputs, a stack block), or
+      // - A reference to a shadow block, though they aren't explicitly written
+      //   as such; they use a compressed form which is expanded into actual
+      //   shadow blocks. See compressInputTree in scratch-vm's sb3
+      //   serialization code.
+      switch (input.type) {
+        case "blocks":
+          if (input.value.length) {
+            inputs[key] = [2, input.value[0].id];
+            applyBlockData(blockData, serializeBlock(input.value[0], {parent: block, siblingBlocks: input.value, stage, target}));
+          }
+          break;
+        case "block":
+          input[key] = [3, input.value.id, [4, "0"]];
+          applyBlockData(blockData, serializeBlock(input.value, {parent: block, stage, target}));
+          break;
+        default: {
+          let value: sb3.BlockInputValue;
+          switch (input.type) {
+            case "costume":
+              value = makeShadowBlock(OpCode.looks_costume, {COSTUME: [input.value]});
+              break;
+            case "sound":
+              value = makeShadowBlock(OpCode.sound_sounds_menu, {SOUND_MENU: [input.value]});
+              break;
+            case "number":
+              value = [4, input.value.toString()];
+              break;
+          }
+          inputs[key] = [1, value];
+        }
+      }
+    }
+
+    return {inputs, fields, blockData};
+  }
+
+  function serializeBlock(block: Block, options: {
+    stage: Stage,
+    target: Target,
+
+    parent?: Block,
+    siblingBlocks?: Block[],
+    x?: number,
+    y?: number
+  }): BlockData {
+    const result = newBlockData();
+
+    const {parent, siblingBlocks, stage, target} = options;
+
+    let nextBlock;
+    if (siblingBlocks) {
+      const thisIndex = siblingBlocks.indexOf(block) + 1;
+      nextBlock = siblingBlocks.find((x, index) => index === thisIndex);
+    }
+
+    if (nextBlock) {
+      applyBlockData(result, serializeBlock(nextBlock, {parent: block, siblingBlocks, stage, target}));
+    }
+
+    const { inputs, fields, blockData: inputBlockData } = serializeInputs(block, {stage, target});
+    applyBlockData(result, inputBlockData);
+
+    const obj: sb3.Block = {
+      opcode: block.opcode,
+
+      parent: parent ? parent.id : null,
+      next: nextBlock ? nextBlock.id : null,
+      topLevel: !parent,
+
+      inputs,
+      fields,
+
+      shadow: false
+    };
+
+    if (obj.topLevel) {
+      obj.x = options.x;
+      obj.y = options.y;
+    }
+
+    result.blocks[block.id] = obj;
+
+    return result;
+  }
+
+  function serializeTarget(target: Target, options: {stage: Stage}): sb3.Target {
+    const mapToIdObject = (
+      values: Array<{id: string, [propName: string]: any}>,
+      fn: (x: any) => any
+    ): {[key: string]: any} => {
+      const ret = {};
+      for (const object of values) {
+        ret[object.id] = fn(object);
+      }
+      return ret;
+    }
+
+    const {stage} = options;
+
+    const blockData = newBlockData();
+
+    for (const script of target.scripts) {
+      applyBlockData(blockData, serializeBlock(script.blocks[0], {
+        stage, target,
+        siblingBlocks: script.blocks,
+        x: script.x,
+        y: script.y
+      }));
+    }
+
+    const {blocks, broadcasts} = blockData;
+
+    return {
+      name: target.name,
+      isStage: target.isStage,
+
+      currentCostume: target.costumeNumber,
+      layerOrder: target.layerOrder,
+      volume: target.volume,
+
+      blocks,
+      broadcasts,
+
+      // sb-edit doesn't support comments (as of feb 12, 2020)
+      comments: {},
+
+      sounds: target.sounds.map(sound => ({
+        name: sound.name,
+        dataFormat: sound.dataFormat,
+        assetId: sound.md5,
+        md5ext: sound.md5 + "." + sound.ext,
+        sampleCount: sound.sampleCount,
+        rate: sound.sampleRate
+      })),
+
+      costumes: target.costumes.map(costume => ({
+        name: costume.name,
+        assetId: costume.md5,
+        bitmapResolution: costume.bitmapResolution,
+        dataFormat: costume.dataFormat,
+        rotationCenterX: costume.centerX,
+        rotationCenterY: costume.centerY
+      })),
+
+      variables: mapToIdObject(target.variables, ({ name, value, cloud }) => {
+        if (cloud) {
+          return [name, value, cloud]
+        } else {
+          return [name, value];
+        }
       }),
-      {}
-    ),
-    layerOrder: project.stage.layerOrder,
-    textToSpeechLanguage: project.textToSpeechLanguage,
-    blocks: project.stage.blocks.reduce((blocks, block) => {
-      const sb3Block: sb3.Block = {
-        opcode: block.opcode,
-        next: block.next,
-        parent: block.parent,
-        x: 0, // TODO: Retrieve actual x/y
-        y: 0
-      };
-      return { ...blocks, [block.id]: sb3Block };
-    }, {})
+
+      lists: mapToIdObject(target.lists, ({ name, value }) => [name, ...value])
+    };
+  }
+
+  const rotationStyleMap: {[key: string]: "all around" | "left-right" | "don't rotate"} = {
+    "normal": "all around",
+    "leftRight": "left-right",
+    "none": "don't rotate"
   };
 
-  const sprites: sb3.Sprite[] = project.sprites.map(sprite => {
-    const sb3Sprite: sb3.Sprite = {
-      name: sprite.name,
-      costumes: extractCostumes(sprite),
-      sounds: extractSounds(sprite),
-      currentCostume: sprite.costumeNumber,
-      rotationStyle: { normal: "all around", "left-right": "leftRight", none: "don't rotate" }[sprite.rotationStyle],
+  function serializeSprite(sprite: Sprite, options: {stage: Stage}): sb3.Sprite {
+    const {stage} = options;
+    return {
+      ...serializeTarget(sprite, {stage}),
+      isStage: false,
       x: sprite.x,
       y: sprite.y,
       size: sprite.size,
       direction: sprite.direction,
-      visible: sprite.visible,
+      rotationStyle: rotationStyleMap[sprite.rotationStyle],
       draggable: sprite.isDraggable,
-      isStage: false,
-      volume: sprite.volume
+      visible: sprite.visible
     };
-    return sb3Sprite;
-  });
+  }
+
+  interface SerializeStageOptions {
+    tempo: number;
+    textToSpeechLanguage: TextToSpeechLanguage;
+    videoState: "on" | "off";
+    videoTransparency: number;
+  }
+
+  function serializeStage(stage: Stage, options: SerializeStageOptions): sb3.Stage {
+    return {
+      ...serializeTarget(stage, {stage}),
+      isStage: true,
+      tempo: options.tempo,
+      textToSpeechLanguage: options.textToSpeechLanguage,
+      videoState: options.videoState,
+      videoTransparency: options.videoTransparency
+    };
+  }
+
+  function serializeProject(project: Project): sb3.ProjectJSON {
+    return {
+      targets: [
+        serializeStage(project.stage, {
+          tempo: project.tempo,
+          textToSpeechLanguage: project.textToSpeechLanguage,
+          videoState: project.videoOn ? "on" : "off",
+          videoTransparency: project.videoAlpha
+        }),
+        ...project.sprites.map(sprite => serializeSprite(sprite, {stage: project.stage}))
+      ],
+      meta: {
+        semver: "3.0.0"
+      }
+    };
+  }
 
   return {
-    targets: [stage, ...sprites],
-    monitors: extractMonitors([project.stage, ...project.sprites]),
-    meta: "waddup"
+    json: JSON.stringify(serializeProject(project))
   };
 }
-*/
