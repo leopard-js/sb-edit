dcfea413cd1dde0bfeb48294824da35c2b407526
Florrie <towerofnix@gmail.com>
2020-02-28 14:59:49 -0400

    combine Variable and List into Data module

--- src/Data.ts
+++ src/Data.ts
@@ -2,7 +2,7 @@ import { generateId } from "./util/id";
 
 type ScalarValue = number | string | boolean;
 
-export default class Variable {
+export class Variable {
   public name: string;
   public id: string;
 
@@ -31,6 +31,42 @@ export default class Variable {
     sliderMin?: number;
     sliderMax?: number;
     isDiscrete?: boolean;
+  }) {
+    Object.assign(this, options);
+
+    if (!this.id) {
+      this.id = generateId();
+    }
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+}
+
+export class List {
+  public name: string;
+  public value: ScalarValue[];
+
+  public visible = true;
+  public x = 0;
+  public y = 0;
+  public width: number = null;
+  public height: number = null;
+
+  public id: string;
+
+  constructor(options: {
+    name: string;
+    value: ScalarValue[];
+
+    visible?: boolean;
+    x?: number;
+    y?: number;
+    width?: number;
+    height?: number;
+
+    id?: string;
   }) {
     Object.assign(this, options);
 
