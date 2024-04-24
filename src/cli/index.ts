5da3ddf1ad5f199abc0a6bdaad2def60d843928c
Josh Pullen <JoshPullen27@gmail.com>
2024-04-24 14:58:45 -0400

    Code improvements based on feedback from @towerofnix

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,13 +1,12 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import { Command, Option } from "commander";
 import Project from "../Project";
-
-import * as fs from "fs";
-import { resolve } from "path";
-import * as JSZip from "jszip";
-
-import * as chalk from "chalk";
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import JSZip from "jszip";
+import chalk from "chalk";
+import wcwidth from "wcwidth";
 
 const program = new Command();
 
@@ -30,17 +29,62 @@ const options: {
 
 let { input, inputType, output, outputType } = options;
 
-// Infer input type
-if (inputType === undefined) {
-  inputType = "sb3";
+class InferTypeError extends Error {
+  constructor(public readonly stage: "input" | "output", message: string) {
+    super(message);
+    this.name = "InferTypeError";
+    Object.setPrototypeOf(this, InferTypeError.prototype);
+  }
 }
 
-// Infer output type
-if (outputType === undefined) {
-  if (output.endsWith(".zip")) {
-    outputType = "leopard-zip";
-  } else {
-    outputType = "leopard";
+try {
+  // Infer input type
+  if (inputType === undefined) {
+    if (input.endsWith(".sb3")) {
+      inputType = "sb3";
+    } else if (input.endsWith(".sb") || input.endsWith(".sprite")) {
+      throw new InferTypeError("input", "Scratch 1.4 input projects are not currently supported.");
+    } else if (input.endsWith(".sb2") || input.endsWith(".sprite2")) {
+      throw new InferTypeError("input", "Scratch 2.0 input projects are not currently supported.");
+    } else if (input.endsWith(".sprite3")) {
+      throw new InferTypeError("input", "Scratch 3.0 sprite inputs are not currently supported.");
+    } else {
+      throw new InferTypeError("input", "Could not infer input file type.");
+    }
+  }
+
+  // Infer output type
+  if (outputType === undefined) {
+    if (output.endsWith(".zip")) {
+      outputType = "leopard-zip";
+    } else if (output.endsWith(".sb")) {
+      throw new InferTypeError("output", "Scratch 1.4 output projects are not currently supported.");
+    } else if (output.endsWith(".sb2")) {
+      throw new InferTypeError("output", "Scratch 2.0 output projects are not currently supported.");
+    } else if (output.endsWith(".sb3")) {
+      throw new InferTypeError("output", "Scratch 3.0 output projects are not currently supported.");
+    } else if (path.extname(output) === "") {
+      outputType = "leopard";
+    } else {
+      throw new InferTypeError("output", "Could not infer output type.");
+    }
+  }
+} catch (err) {
+  if (err instanceof InferTypeError) {
+    process.stderr.write(chalk.red`${err.message}`);
+    switch (err.stage) {
+      case "input":
+        process.stderr.write(
+          chalk.gray` Please choose a different input file. (Or, if your file is actually of a supported type and just has an unusual file name, specify the correct type with --input-type.)\n`
+        );
+        break;
+      case "output":
+        process.stderr.write(
+          chalk.gray` Please choose a different output path or specify the desired output type with --output-type.\n`
+        );
+        break;
+    }
+    process.exit(1);
   }
 }
 
@@ -55,17 +99,20 @@ async function run() {
 
   let stepNumber = 1;
   async function writeStep<ReturnType>(description: string, fn: () => ReturnType | Promise<ReturnType>) {
-    process.stdout.write(chalk.gray`  ${stepNumber++}. ${description}`);
+    const thisStepNumber = stepNumber++;
+    process.stdout.write(chalk.gray`  ${thisStepNumber}. ${description}`);
     let value: ReturnType;
     try {
       value = await fn();
       process.stdout.write(chalk.bold.green(" Done.\n"));
     } catch (err) {
       if (err instanceof StepError) {
-        process.stdout.write(chalk.bold.red(` ${err.message}`));
+        process.stdout.write(chalk.bold.red(`\n${" ".repeat(4 + String(thisStepNumber).length)}${err.message}`));
         process.stdout.write(chalk.red(`\n\nProject conversion failed.\n`));
       } else {
-        process.stdout.write(chalk.bold.red` An unknown error occurred.`);
+        process.stdout.write(
+          chalk.bold.red`\n${" ".repeat(4 + String(thisStepNumber).length)}An unknown error occurred.`
+        );
         process.stderr.write(chalk.red`\n\n${err}\n`);
       }
       process.exit(1);
@@ -75,14 +122,14 @@ async function run() {
 
   process.stdout.write(chalk.underline.gray`Converting project:\n`);
 
-  const fullInputPath = resolve(process.cwd(), input);
+  const fullInputPath = path.resolve(process.cwd(), input);
 
   const project = await writeStep(
     `${chalk.bold("Importing")} ${inputType} project from path ${chalk.white(fullInputPath)}.`,
     async () => {
       let file: Buffer;
       try {
-        file = fs.readFileSync(fullInputPath);
+        file = await fs.readFile(fullInputPath);
       } catch (err) {
         if (err instanceof Object && "code" in err) {
           switch (err.code) {
@@ -94,7 +141,14 @@ async function run() {
         }
         throw err;
       }
-      const project = await Project.fromSb3(file);
+
+      let project: Project;
+      switch (inputType) {
+        case "sb3": {
+          project = await Project.fromSb3(file);
+          break;
+        }
+      }
       return project;
     }
   );
@@ -102,23 +156,22 @@ async function run() {
   switch (outputType) {
     case "leopard": {
       const leopard = await writeStep(`${chalk.bold("Converting")} project to ${chalk.white("Leopard")}.`, () => {
-        const leopard = project.toLeopard();
-        return leopard;
+        return project.toLeopard();
       });
 
-      const fullOutputPath = resolve(process.cwd(), output);
+      const fullOutputPath = path.resolve(process.cwd(), output);
 
-      await writeStep(`${chalk.bold("Exporting")} project to directory ${chalk.white(fullOutputPath)}.`, () => {
+      await writeStep(`${chalk.bold("Exporting")} project to directory ${chalk.white(fullOutputPath)}.`, async () => {
         // First, make sure the output path is an empty directory (create it if it doesn't exist)
         let existingFiles: string[];
         try {
-          existingFiles = fs.readdirSync(fullOutputPath);
+          existingFiles = await fs.readdir(fullOutputPath);
         } catch (err) {
           if (err instanceof Object && "code" in err) {
             switch (err.code) {
               case "ENOENT":
                 // Directory does not exist, create it
-                fs.mkdirSync(output, { recursive: true });
+                await fs.mkdir(output, { recursive: true });
                 existingFiles = [];
                 break;
               case "ENOTDIR":
@@ -137,26 +190,30 @@ async function run() {
 
         // Write the files
         for (const [filename, contents] of Object.entries(leopard)) {
+          const filePath = path.resolve(fullOutputPath, filename);
+
           // Create directories as needed
-          fs.mkdirSync(resolve(fullOutputPath, filename, ".."), { recursive: true });
+          await fs.mkdir(path.dirname(filePath), { recursive: true });
 
           // Write the file
-          fs.writeFileSync(resolve(fullOutputPath, filename), contents);
+          await fs.writeFile(filePath, contents);
         }
 
         for (const target of [project.stage, ...project.sprites]) {
+          const costumeDir = path.join(target.name, "costumes");
           for (const costume of target.costumes) {
-            const filename = `${target.name}/costumes/${costume.name}.${costume.ext}`;
+            const filename = path.join(costumeDir, `${costume.name}.${costume.ext}`);
             const asset = Buffer.from(costume.asset as ArrayBuffer);
-            fs.mkdirSync(resolve(fullOutputPath, filename, ".."), { recursive: true });
-            fs.writeFileSync(resolve(fullOutputPath, filename), asset);
+            await fs.mkdir(path.resolve(fullOutputPath, filename, ".."), { recursive: true });
+            await fs.writeFile(path.resolve(fullOutputPath, filename), asset);
           }
 
+          const soundDir = path.join(target.name, "sounds");
           for (const sound of target.sounds) {
-            const filename = `${target.name}/sounds/${sound.name}.${sound.ext}`;
+            const filename = path.join(soundDir, `${sound.name}.${sound.ext}`);
             const asset = Buffer.from(sound.asset as ArrayBuffer);
-            fs.mkdirSync(resolve(fullOutputPath, filename, ".."), { recursive: true });
-            fs.writeFileSync(resolve(fullOutputPath, filename), asset);
+            await fs.mkdir(path.resolve(fullOutputPath, filename, ".."), { recursive: true });
+            await fs.writeFile(path.resolve(fullOutputPath, filename), asset);
           }
         }
       });
@@ -165,9 +222,9 @@ async function run() {
       process.stdout.write(chalk.gray` Files written to ${chalk.white(fullOutputPath)}\n\n`);
       process.stdout.write(
         chalkBox([
-          { str: "To preview the project, run:" },
-          { str: `$ ${chalk.white`cd ${output}`}`, length: 5 + output.length },
-          { str: `$ ${chalk.white`npx vite`}`, length: 10 }
+          "To preview the project, run:",
+          `$ ${chalk.white`cd ${output}`}`,
+          `$ ${chalk.white`npx vite`} # or serve with any HTTP server`
         ])
       );
       break;
@@ -178,12 +235,12 @@ async function run() {
         return leopard;
       });
 
-      const fullOutputPath = resolve(process.cwd(), output);
+      const fullOutputPath = path.resolve(process.cwd(), output);
 
-      await writeStep(`${chalk.bold("Exporting")} project to zip file ${chalk.white(fullOutputPath)}.`, () => {
+      await writeStep(`${chalk.bold("Exporting")} project to zip file ${chalk.white(fullOutputPath)}.`, async () => {
         // First, check if file name is already taken
         try {
-          fs.accessSync(fullOutputPath);
+          await fs.access(fullOutputPath);
           throw new StepError("Output file already exists.");
         } catch (err) {
           if (err instanceof Object && "code" in err && err.code === "ENOENT") {
@@ -213,7 +270,9 @@ async function run() {
           }
         }
 
-        zip.generateNodeStream({ type: "nodebuffer", streamFiles: true }).pipe(fs.createWriteStream(fullOutputPath));
+        zip
+          .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+          .pipe((await fs.open(fullOutputPath)).createWriteStream());
       });
 
       break;
@@ -221,16 +280,16 @@ async function run() {
   }
 }
 
-function chalkBox(lines: { str: string; length?: number }[]): string {
-  const lengths = lines.map(line => line.length ?? line.str.length);
+function chalkBox(lines: string[]): string {
+  const lengths = lines.map(line => wcwidth(line));
   const boxInnerWidth = Math.max(...lengths) + 2;
 
   let outputStr = "";
   outputStr += chalk.bold.blue`╔${"═".repeat(boxInnerWidth)}╗`;
   for (const line of lines) {
-    const length = line.length ?? line.str.length;
+    const length = wcwidth(line);
     outputStr += chalk.bold.blue`\n║ `;
-    outputStr += chalk.gray(line.str);
+    outputStr += chalk.gray(line);
     outputStr += " ".repeat(boxInnerWidth - length - 2);
     outputStr += chalk.bold.blue` ║`;
   }
